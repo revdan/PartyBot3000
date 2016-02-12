00165192142572946c819ecb0349daa67fab9d61
Dan Rees-Jones <dan@vzaar.com>
2016-02-12 14:20:45 +0000

    formatting

--- lib/party_bot_3000/terry.ex
+++ lib/party_bot_3000/terry.ex
@@ -1,11 +1,11 @@
-defmodule PartyBot3000.Terry do
+defmodule PartyBot3001.Terry do
 
   @prefix ~w(bloody fucking shitting twatting tossing spunking)
   @object ~w(fuck shit arse toss spunk turd)
   @intensifier ~w(
     Avalanche Blizzard Cyclone Flood Hail Lightning Monsoon Storm Thunderstorm Tornado
     bomb pile heap pyramid jumble mound stack bunch hoard load cube sphere
-    )
+  )
 
   def fy do
     [optional_prefix, sample(@object), sample(@intensifier), optional_wonder]
